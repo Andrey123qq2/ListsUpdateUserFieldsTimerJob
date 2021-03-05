1499b697dd8f492f99d0119914716cef76c46a29
Andrey Nesterov <and123qq2@gmail.com>
2021-03-05 13:02:59 +0200

    Add project files.

---
+++ Features/Feature1/Feature1.EventReceiver.cs
@@ -0,0 +1,131 @@
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+namespace ListsUpdateUserFieldsTimerJob.Features.Feature1
+{
+    /// <summary>
+    /// This class handles events raised during feature activation, deactivation, installation, uninstallation, and upgrade.
+    /// </summary>
+    /// <remarks>
+    /// The GUID attached to this class may be used during packaging and should not be modified.
+    /// </remarks>
+
+    [Guid("1ca3b581-cd4f-446e-b609-959cec52626e")]
+    public class Feature1EventReceiver : SPFeatureReceiver
+    {
+        const string JobName = "Lists Update User Fields TimerJob";
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            try
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate ()
+                {
+                    SPWebApplication parentWebApp = (SPWebApplication)properties.Feature.Parent;
+                    DeleteExistingJob(JobName, parentWebApp);
+                    CreateJob(parentWebApp);
+                });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private bool CreateJob(SPWebApplication site)
+        {
+            bool jobCreated = false;
+            try
+            {
+                TimerJob job = new TimerJob(JobName, site);
+                job.Schedule = new SPDailySchedule()
+                {
+                    BeginHour = 7,
+                    EndHour = 8,
+                    BeginMinute = 0,
+                    EndMinute = 15,
+                };
+
+                job.Update();
+            }
+            catch (Exception)
+            {
+                return jobCreated;
+            }
+            return jobCreated;
+        }
+        public bool DeleteExistingJob(string jobName, SPWebApplication site)
+        {
+            bool jobDeleted = false;
+            try
+            {
+                foreach (SPJobDefinition job in site.JobDefinitions)
+                {
+                    if (job.Name == jobName)
+                    {
+                        job.Delete();
+                        jobDeleted = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return jobDeleted;
+            }
+            return jobDeleted;
+        }
+
+        public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        {
+
+            lock (this)
+            {
+                try
+                {
+                    SPSecurity.RunWithElevatedPrivileges(delegate ()
+                    {
+                        SPWebApplication parentWebApp = (SPWebApplication)properties.Feature.Parent;
+                        DeleteExistingJob(JobName, parentWebApp);
+                    });
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+        // Uncomment the method below to handle the event raised after a feature has been activated.
+
+        //public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised before a feature is deactivated.
+
+        //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised after a feature has been installed.
+
+        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+
+        // Uncomment the method below to handle the event raised before a feature is uninstalled.
+
+        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        //{
+        //}
+
+        // Uncomment the method below to handle the event raised when a feature is upgrading.
+
+        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        //{
+        //}
+    }
+}
---
+++ TimerJob.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SharePoint.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsUpdateUserFieldsTimerJob
+{
+    class TimerJob : SPJobDefinition
+    {
+        public TimerJob() : base() { }
+        public TimerJob(string jobName, SPService service, SPServer server, SPJobLockType targetType) : base(jobName, service, server, targetType) { }
+        public TimerJob(string jobName, SPWebApplication webApplication) : base(jobName, webApplication, null, SPJobLockType.ContentDatabase)
+        {
+            this.Title = jobName;
+        }
+        public override void Execute(Guid contentDbId)
+        {
+            try { } catch (Exception ex) { }
+        }
+    }
+}
